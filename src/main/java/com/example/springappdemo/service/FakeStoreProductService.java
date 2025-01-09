d6653554573e706431d2b63ae5805886e41ccfac
Brindha <brindha.188@gmail.com>
2025-01-09 15:45:51 +0530

    Dockerize product service. Commented Redis caching code to reduce dependencies for docker integration.

--- src/main/java/com/example/springappdemo/service/FakeStoreProductService.java
+++ src/main/java/com/example/springappdemo/service/FakeStoreProductService.java
@@ -5,7 +5,7 @@ import com.example.springappdemo.exception.ProductNotFoundException;
 import com.example.springappdemo.model.Category;
 import com.example.springappdemo.model.Product;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -22,20 +22,20 @@ import java.util.List;
 public class FakeStoreProductService implements ProductService {
 
     RestTemplate restTemplate;
-    RedisTemplate<String, Object> redisTemplate;
+    //RedisTemplate<String, Object> redisTemplate;
 
-    public FakeStoreProductService(RestTemplate restTemplate, RedisTemplate redisTemplate) {
+    public FakeStoreProductService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.redisTemplate = redisTemplate;
+        //this.redisTemplate = redisTemplate;
     }
 
     @Override
     public Product getProductById(long id) throws ProductNotFoundException {
-        Product product = (Product) redisTemplate.opsForHash().get("PRODUCTS", "PRODUCT_" + id);
-        if (product != null) {
-            System.out.println("Product found in Redis");
-            return product;
-        }
+//        Product product = (Product) redisTemplate.opsForHash().get("PRODUCTS", "PRODUCT_" + id);
+//        if (product != null) {
+//            System.out.println("Product found in Redis");
+//            return product;
+//        }
         FakeStoreProductDto dto = restTemplate.getForObject("https://fakestoreapi.com/products/" + id, FakeStoreProductDto.class);
         // Creating FakeStoreProductDto for testing redis because the api service is down
 //        FakeStoreProductDto dto = new FakeStoreProductDto();
@@ -47,8 +47,8 @@ public class FakeStoreProductService implements ProductService {
 
         if(dto == null)
             throw new ProductNotFoundException(100L, "Product not found for id " + id);
-        product = convertFakeStoreProductDtoToProduct(dto);
-        redisTemplate.opsForHash().put("PRODUCTS", "PRODUCT_" + id, product);
+        Product product = convertFakeStoreProductDtoToProduct(dto);
+        //redisTemplate.opsForHash().put("PRODUCTS", "PRODUCT_" + id, product);
         return product;
     }
 
